f2d77ebbb4550e5b749283495e5cb2cd433cc8b9
yevheniy73 <71726440+yevheniy73@users.noreply.github.com>
2023-05-15 00:47:54 -0600

    hero section code

--- app/layout.js
+++ app/layout.js
@@ -7,13 +7,16 @@ const RootLayout = ({ children }) => (
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
       <link href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@400;500;600;700&display=swap" rel="stylesheet" />
-    </head>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.4.0/styles/atom-one-dark.min.css" />
+      </head>
     <body>
       <div id='portfolio' className='flex flex-col justify-between'>
         <Navbar />
         {children}
         <Footer />
       </div>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.4.0/highlight.min.js"></script>
+      <script>hljs.highlightAll();</script>
     </body>
   </html>
 );
--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -1,18 +1,31 @@
 'use client';
 
+const message = `<div className="head z-10">
+  <span className="text-white text-[18px]">Hi, I am</span>
+  <h1 className="text-white text-[58px]">Yevhen Kaznovskyi</h1>
+  <h2 className="text-purple text-[32px]">Software developer</h2>
+</div>`;
+
 const Hero = () => (
+ 
   <section id="hero" className="w-full flex items-center justify-between gap-8">
     <div>
-    <div className="css-blurry-gradient-blue"></div>
-    <div className="css-blurry-gradient-green"></div>
-      <div class="head">
-        <span className="text-white text-[18px]"> Hi, I am </span>
+      <div className="css-blurry-gradient-blue"></div>
+      <div className="css-blurry-gradient-green"></div>
+      <div className="head z-10">
+        <span className="text-white text-[18px]">Hi, I am</span>
         <h1 className="text-white text-[58px]">Yevhen Kaznovskyi</h1>
-        <h2 className=" text-secondary-purple text-[32px]">&gt; Software developer</h2>
+        <h2 className="text-secondary-purple text-[32px]">&gt; Software developer</h2>
       </div>
     </div>
-    <div>
+    <div className="z-10">
+      <div className="flex justify-center items-center w-[580px] h-[185px] p-2 bg-primary-dark rounded-[12px] border-[1px] border-primary-blue text-[12px]">
+        <pre>
+          <code>{message}</code>
+        </pre> 
+      </div>
     </div>
+
   </section>
 );
 
