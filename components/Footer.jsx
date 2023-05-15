5ee2d734b16ac8ed2eca1ac39dca61e7f727f012
yevheniy73 <71726440+yevheniy73@users.noreply.github.com>
2023-05-14 20:50:04 -0600

    errors in build

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -5,17 +5,18 @@ const Footer = () => (
   <div className="w-full hidden lg:flex flex-col">
     <footer className="w-full h-[56px] flex justify-between items-center border-top text-secondary-gray">
       <div className="flex h-full">
-        <p className='min-w-[100px]'>
+        <p className="min-w-[100px]">
           find me on:
         </p>
-        <a href="https://www.linkedin.com/in/kaznovskyi/" target="_blank" className="p-0 w-[56px]">
+        <a href="https://www.linkedin.com/in/kaznovskyi/" target="_blank" rel="noreferrer" className="p-0 w-[56px]">
           <i className="ri-linkedin-box-fill w-[55px] h-full"></i>
         </a>
       </div>
-      <a href="https://github.com/yevheniy73" target="_blank" className='border-left'>
+      <a href="https://github.com/yevheniy73" target="_blank" rel="noreferrer" className="border-left">
         @yevheniy73
       </a>
     </footer>
   </div>
 );
+
 export default Footer;
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { usePathname } from 'next/navigation';
+import { usePathname } from "next/navigation";
 
 const Navbar = () => {
   const pathname = usePathname();
@@ -9,20 +9,20 @@ const Navbar = () => {
     <div className="w-full hidden lg:flex flex-col">
       <nav id="desktop-nav" className="w-full h-[56px] flex justify-between items-center border-bot text-secondary-gray">
         <div className="flex h-full">
-          <a href="/" className='min-w-[300px]'>
+          <a href="/" className="min-w-[300px]">
             _yevhen-kaznovskyi
           </a>
-          <a href="/" className={pathname === '/' ? 'active' : ''}>
+          <a href="/" className={pathname === "/" ? "active" : ""}>
             _hello
           </a>
-          <a href="/about-me" className={pathname.startsWith('/about-me') ? 'active' : ''}>
+          <a href="/about-me" className={pathname.startsWith("/about-me") ? "active" : ""}>
             _about-me
           </a>
-          <a href="/projects" className={pathname.startsWith('/projects') ? 'active' : ''}>
+          <a href="/projects" className={pathname.startsWith("/projects") ? "active" : ""}>
             _projects
           </a>
         </div>
-        <a href="/contact-me" className={`${pathname.startsWith('/contact-me') ? 'active' : ''} border-left`}>
+        <a href="/contact-me" className={`${pathname.startsWith("/contact-me") ? "active" : ""} border-left`}>
           _contact-me
         </a>
       </nav>
