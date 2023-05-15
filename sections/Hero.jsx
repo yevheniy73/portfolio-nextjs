59978063c3c9476b4adc69534049d308d3c5e8de
yevheniy73 <71726440+yevheniy73@users.noreply.github.com>
2023-05-15 01:01:38 -0600

    hero section code cards

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -6,6 +6,12 @@ const message = `<div className="head z-10">
   <h2 className="text-purple text-[32px]">Software developer</h2>
 </div>`;
 
+const message2 = `#desktop-nav a:hover,
+#desktop-nav a:focus {
+  color: #fff;
+  background: #1E2D3D;
+}`
+
 const Hero = () => (
  
   <section id="hero" className="w-full flex items-center justify-between gap-8">
@@ -18,8 +24,13 @@ const Hero = () => (
         <h2 className="text-secondary-purple text-[32px]">&gt; Software developer</h2>
       </div>
     </div>
-    <div className="z-10">
-      <div className="flex justify-center items-center w-[580px] h-[185px] p-2 bg-primary-dark rounded-[12px] border-[1px] border-primary-blue text-[12px]">
+    <div className="z-10 relative">
+      <div className="flex justify-start items-center w-[540px] h-[175px] p-8 bg-primary-dark rounded-[12px] border-[1px] border-primary-blue text-[12px] opacity-70 -mt-[120px] left-16 absolute z-0">
+        <pre>
+          <code>{message2}</code>
+        </pre> 
+      </div>
+      <div className="flex justify-center items-center w-[540px] h-[175px] p-2 bg-primary-dark rounded-[12px] border-[1px] border-primary-blue text-[12px] relative z-1">
         <pre>
           <code>{message}</code>
         </pre> 
