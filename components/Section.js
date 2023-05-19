166f4753e0de82a9a4baaab604af0ac1636b16f2
yevheniy73 <71726440+yevheniy73@users.noreply.github.com>
2023-05-18 23:35:34 -0600

    section component build error

--- components/Section.js
+++ components/Section.js
@@ -1,13 +1,11 @@
 import React from 'react';
 
 const Section = ({ iconChev = './ChevronRight.svg', iconFolder, buttonText, buttonId }) => (
-  <>
-    <button id={buttonId} type="button" className="w-full h-[28px] text-left flex flex-row justify-start items-center gap-[10px] pl-[16px]">
-      <img src={iconChev} alt="Chevron" className="icon" />
-      <img src={iconFolder} alt="Folder" className="icon" />
-      {buttonText}
-    </button>
-  </>
+  <button id={buttonId} type="button" className="w-full h-[28px] text-left flex flex-row justify-start items-center gap-[10px] pl-[16px]">
+    <img src={iconChev} alt="Chevron" className="icon" />
+    <img src={iconFolder} alt="Folder" className="icon" />
+    {buttonText}
+  </button>
 );
 
 export default Section;
