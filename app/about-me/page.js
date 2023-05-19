29aee15729524676a6d7a03cfae989192ad8f6a0
yevheniy73 <71726440+yevheniy73@users.noreply.github.com>
2023-05-18 22:19:51 -0600

    completely fixed the resizer issues

--- app/about-me/page.js
+++ app/about-me/page.js
@@ -1,131 +1,118 @@
 'use client';
 
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useRef, createRef, useState } from 'react';
+import IconBarButton from '../../components/IconBarButton';
+import ContentButton from '../../components/ContentButton';
 
 const About = () => {
-
     const [activeButton, setActiveButton] = useState(2);
-    const [content1Visible, setContent1Visible] = useState(false);
-    const [content2Visible, setContent2Visible] = useState(true);
-    const [content3Visible, setContent3Visible] = useState(false);
-
-    const handleButtonClick = (buttonIndex) => {
-        setActiveButton(buttonIndex);
-        if (buttonIndex === 1) {
-            setContent1Visible(true);
-            setContent2Visible(false);
-            setContent3Visible(false);
-        } else if (buttonIndex === 2) {
-            setContent1Visible(false);
-            setContent2Visible(true);
-            setContent3Visible(false);
-        } else if (buttonIndex === 3) {
-            setContent1Visible(false);
-            setContent2Visible(false);
-            setContent3Visible(true);
-        }
-    };
-
+    const [contentVisible, setContentVisible] = useState([false, true, false]);
     const [lines, setLines] = useState(0);
-
+    const textContentRefs = useRef([]);
+    const [initialLoad, setInitialLoad] = useState(true);
+  
+    // Initialize the refs for each <span> element
+    useEffect(() => {
+      textContentRefs.current = Array(3)
+        .fill()
+        .map((_, index) => textContentRefs.current[index] || createRef());
+  
+      // Simulate a click on the second section to activate it after the component mounts
+      if (initialLoad) {
+        handleButtonClick(2);
+        setInitialLoad(false);
+      }
+    }, [initialLoad]);
+  
+    const handleButtonClick = (buttonIndex) => {
+      setActiveButton(buttonIndex);
+      const newContentVisible = contentVisible.map((_, index) => index + 1 === buttonIndex);
+      setContentVisible(newContentVisible);
+      handleResize();
+    };
+  
     const handleResize = () => {
-        const textElement = document.getElementById("text-content");
+      const textElement = textContentRefs.current[activeButton - 1]?.current;
+      if (textElement) {
         const lineHeight = parseInt(getComputedStyle(textElement).lineHeight);
         const elementHeight = textElement.offsetHeight;
         const newLines = Math.ceil(elementHeight / lineHeight);
         setLines(newLines);
+      }
     };
-
+  
     useEffect(() => {
-        const textElement = document.getElementById("text-content");
-        const resizeObserver = new ResizeObserver(entries => {
-            handleResize();
-        });
-
+      const resizeObserver = new ResizeObserver(() => {
+        handleResize();
+      });
+  
+      const textElement = textContentRefs.current[activeButton - 1]?.current;
+      if (contentVisible[activeButton - 1] && textElement) {
         resizeObserver.observe(textElement);
-
-        window.addEventListener("resize", handleResize);
-
-        return () => {
-            resizeObserver.disconnect();
-            window.removeEventListener("resize", handleResize);
-        };
-    }, []);
-
-    const gridItems = [];
-
-    for (let i = 1; i <= lines + 1; i++) {
-        let content = '*';
-
-        if (i === 1) {
-            content = '/**'; // Replacing the first *
-        } else if (i === lines + 1) {
-            content = '*/'; // Replacing the last *
+      }
+  
+      window.addEventListener('resize', handleResize);
+  
+      handleResize();
+  
+      return () => {
+        if (textElement) {
+          resizeObserver.unobserve(textElement);
         }
-
-        gridItems.push(
-            <React.Fragment key={i}>
-                <div className="grid-cell">{i}</div>
-                <div className="grid-cell">{content}</div>
-            </React.Fragment>
-        );
+        window.removeEventListener('resize', handleResize);
+      };
+    }, [activeButton, contentVisible]);
+  
+    const gridItems = [];
+  
+    for (let i = 1; i <= lines + 1; i++) {
+      let content = '*';
+  
+      if (i === 1) {
+        content = '/**'; // Replacing the first *
+      } else if (i === lines + 1) {
+        content = '*/'; // Replacing the last *
+      }
+  
+      gridItems.push(
+        <React.Fragment key={i}>
+          <div className="grid-cell">{i}</div>
+          <div className="grid-cell">{content}</div>
+        </React.Fragment>
+      );
     }
 
     return (
         <div className="w-full h-full flex flex-row">
-            {/* =======================================================================================*/}
             <div id="sideContainer" className="flex flex-row max-w-[310px] min-w-[310px] border-right">
-
-                {/*about-me page left icon bar start -----------------------*/}
                 <div id="aboutSideBar" className="max-w-[70px] min-w-[70px] h-full border-right">
-                    <button
-                        type="button"
-                        className={`p-0 w-full h-[50px] opacity-40 ${activeButton === 1 ? 'active' : ''}`}
+                    <IconBarButton
+                        active={activeButton === 1}
+                        icon="ri-terminal-box-fill"
                         onClick={() => handleButtonClick(1)}
-                    >
-                        <i className="ri-terminal-box-fill" />
-                    </button>
-
-                    <button
-                        type="button"
-                        className={`p-0 w-full h-[50px] opacity-40 ${activeButton === 2 ? 'active' : ''}`}
+                    />
+                    <IconBarButton
+                        active={activeButton === 2}
+                        icon="ri-game-fill"
                         onClick={() => handleButtonClick(2)}
-                    >
-                        <i className="ri-game-fill" />
-                    </button>
-
-                    <button
-                        type="button"
-                        className={`p-0 w-full h-[50px] opacity-40 ${activeButton === 3 ? 'active' : ''}`}
+                    />
+                    <IconBarButton
+                        active={activeButton === 3}
+                        icon="ri-gamepad-fill"
                         onClick={() => handleButtonClick(3)}
-                    >
-                        <i className="ri-gamepad-fill" />
-                    </button>
+                    />
                 </div>
-                {/*about-me page left icon bar end -------------------------*/}
-
-                {/*folder drop-down start-----------------------------------*/}
-                {content1Visible && (
+                {contentVisible[0] && (
                     <div className="flex flex-col w-full">
-                        <button id="personalInfoBtn" type="button" className="w-full h-[40px] border-bot text-left px-4 text-white">
-                            personal-info
-                        </button>
-                        <div className="w-full h-[190px] border-bot">
-                        </div>
-                        <button id="contactsBtn" type="button" className="w-full h-[40px] border-bot text-left px-4 text-white">
-                            contacts
-                        </button>
+                        <ContentButton onClick={() => { }} text="personal-info" />
+                        <div className="w-full h-[190px] border-bot"></div>
+                        <ContentButton onClick={() => { }} text="contacts" />
                         <div className="w-full flex-grow"></div>
                     </div>
                 )}
-                {content2Visible && (
-                    <div className="flex flex-col w-full">    
-
-                        <button id="personalInfoBtn" type="button" className="w-full h-[40px] border-bot text-left px-4 text-white">
-                            profession-info
-                        </button>
-
-
+                {contentVisible[1] && (
+                    <div className="flex flex-col w-full">
+                        <ContentButton onClick={() => { }} text="profession-info" />
                         <div className="my-drop-down block w-full h-[190px] border-bot">
                             <button id="pro1" type="button" className="w-full h-[28px] text-left px-4">
                                 bio
@@ -137,60 +124,42 @@ const About = () => {
                                 education
                             </button>
                         </div>
-
-
-                        <button id="contactsBtn" type="button" className="w-full h-[40px] border-bot text-left px-4 text-white">
-                            contacts
-                        </button>
+                        <ContentButton onClick={() => { }} text="contacts" />
                         <div className="w-full flex-grow"></div>
-
                     </div>
                 )}
-                {content3Visible && (
+                {contentVisible[2] && (
                     <div className="flex flex-col w-full">
-                        <button id="personalInfoBtn" type="button" className="w-full h-[40px] border-bot text-left px-4 text-white">
-                            hobbies-info
-                        </button>
-                        <div className="w-full h-[190px] border-bot">
-                        </div>
-                        <button id="contactsBtn" type="button" className="w-full h-[40px] border-bot text-left px-4 text-white">
-                            contacts
-                        </button>
+                        <ContentButton onClick={() => { }} text="hobbies-info" />
+                        <div className="w-full h-[190px] border-bot"></div>
+                        <ContentButton onClick={() => { }} text="contacts" />
                         <div className="w-full flex-grow"></div>
                     </div>
                 )}
-                {/*folder drop-down end-------------------------------------*/}
-
             </div>
-            {/* =======================================================================================*/}
-            <div className="flex flex-row w-full">
 
-                {content1Visible && (
+            <div className="flex flex-row w-full">
+                {contentVisible[0] && (
                     <div className="w-1/2 border-right flex flex-col">
-                        <div className="w-full h-[40px] border-bot">
-                            {/* Content here */}
-                        </div>
+                        <div className="w-full h-[40px] border-bot">{/* Content here */}</div>
                         <div className="flex flex-row flex-grow w-full gap-4 py-[20px] px-[35px]">
+                            <div className="grid min-w-[64px]">{gridItems}</div>    
                             <div className="w-full">
-                                {/* Content here */}
-                                TODO: profession-info
+                                <span ref={textContentRefs.current[0]} className={`text-content${contentVisible[0] ? ' visible' : ''}`}>
+                                    TODO: profession-info
+                                </span>
                             </div>
                         </div>
                     </div>
                 )}
-                {content2Visible && (
+                {contentVisible[1] && (
                     <div className="w-1/2 border-right flex flex-col">
-                        <div className="w-full h-[40px] border-bot">
-                            {/* Content here */}
-                        </div>
+                        <div className="w-full h-[40px] border-bot">{/* Content here */}</div>
                         <div className="flex flex-row flex-grow w-full gap-4 py-[20px] px-[35px]">
-
-                            <div className="grid min-w-[64px]">
-                                {gridItems}
-                            </div>
-
+                            <div className="grid min-w-[64px]">{gridItems}</div>
                             <div className="w-full">
-                                <span id="text-content">
+                                {/* Add "visible" class based on contentVisible */}
+                                <span ref={textContentRefs.current[1]} className={`text-content${contentVisible[1] ? ' visible' : ''}`}>
                                     <br></br>
                                     Hi, I'm Yevhen! I'm a final year Honors Computing Science student at the University of Alberta,
                                     with over a year of professional experience as a software developer. I am passionate about coding
@@ -201,27 +170,27 @@ const About = () => {
                         </div>
                     </div>
                 )}
-                {content3Visible && (
+                {contentVisible[2] && (
                     <div className="w-1/2 border-right flex flex-col">
-                        <div className="w-full h-[40px] border-bot">
-                            {/* Content here */}
-                        </div>
+                        <div className="w-full h-[40px] border-bot">{/* Content here */}</div>
                         <div className="flex flex-row flex-grow w-full gap-4 py-[20px] px-[35px]">
+                            <div className="grid min-w-[64px]">{gridItems}</div>  
                             <div className="w-full">
-                                {/* Content here */}
-                                TODO: hobbies-info
+                                <span ref={textContentRefs.current[2]} className={`text-content${contentVisible[2] ? ' visible' : ''}`}>
+                                    <br></br>
+                                    Hi, I'm Yevhen! I'm a final year Honors Computing Science student at the University of Alberta,
+                                    with over a year of professional experience as a software developer. I am passionate about coding
+                                </span>   
                             </div>
                         </div>
                     </div>
                 )}
 
-                {/* ---------------------------------------------------------------------------------------*/}
                 <div className="w-1/2 border-right flex flex-col">
-                    <div className="w-full h-[40px] border-bot">
-                    </div>
+                    <div className="w-full h-[40px] border-bot"></div>
                     <div className="flex flex-row flex-grow w-full gap-4 py-[20px] px-[35px]">
                         <div className="w-full">
-                            // Code snippet showcase:
+                            {/* Code snippet showcase */}
                         </div>
                     </div>
                 </div>
---
+++ components/ContentButton.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ContentButton = ({ onClick, text }) => (
+  <button type="button" className="w-full h-[40px] border-bot text-left px-4 text-white" onClick={onClick}>
+    {text}
+  </button>
+);
+
+export default ContentButton;
