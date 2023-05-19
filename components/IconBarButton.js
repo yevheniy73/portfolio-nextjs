f3dc9ff14b6e76d4eb09a2a3055b6a87896aa496
yevheniy73 <71726440+yevheniy73@users.noreply.github.com>
2023-05-18 22:24:43 -0600

    build error on new components 2

--- components/IconBarButton.js
+++ components/IconBarButton.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
 const IconBarButton = ({ active, icon, onClick }) => (
-    <button
-        type="button"
-        className={`p-0 w-full h-[50px] opacity-40 ${active ? 'active' : ''}`}
-        onClick={onClick}
-    >
-        <i className={icon} />
-    </button>
+  <button
+    type="button"
+    className={`p-0 w-full h-[50px] opacity-40 ${active ? 'active' : ''}`}
+    onClick={onClick}
+  >
+    <i className={icon} />
+  </button>
 );
 
 export default IconBarButton;
