8f2d925bc09022e69c91069da448c7530195cb23
yevheniy73 <71726440+yevheniy73@users.noreply.github.com>
2023-05-18 22:25:28 -0600

    build error on new components 3

--- components/ContentButton.js
+++ components/ContentButton.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ContentButton = ({ onClick, text }) => (
   <button type="button" className="w-full h-[40px] border-bot text-left px-4 text-white" onClick={onClick}>
-      {text}
+    {text}
   </button>
 );
 
