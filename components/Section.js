c4735d8fcbe830e85bfe546a7302c660da2cfcf2
yevheniy73 <71726440+yevheniy73@users.noreply.github.com>
2023-05-21 00:17:18 -0600

    trailing space error

--- components/Section.js
+++ components/Section.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Section = ({ iconChev = './ChevronRight.svg', iconFolder, buttonText, buttonId, onClick, active }) => (
-  <button 
+  <button
     id={buttonId}
     type="button"
     className={`${active ? 'active' : ''} w-full h-[28px] text-left flex flex-row justify-start items-center gap-[10px] pl-[16px]`}
