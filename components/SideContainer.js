a1eb1e1c3170f99a937eea5155126d3036c525a1
yevheniy73 <71726440+yevheniy73@users.noreply.github.com>
2023-05-22 23:56:47 -0600

    resume hidden

--- components/SideContainer.js
+++ components/SideContainer.js
@@ -120,7 +120,7 @@ const SideContainer = ({ activeButton, handleButtonClick, handleSubClick, conten
       <ContentButton text="contacts" />
       <div className="my-contacts w-full flex flex-col flex-grow px-[16px] py-[10px]">
         <a href="mailto:kaznovsk@ualberta.ca" className="w-full h-[40px] text-left flex flex-row justify-start items-center">kaznovsk@ualberta.ca</a>
-        <a href="/Yevhen_Kaznovskyi_CV2023.pdf" className="w-full h-[40px] text-left flex flex-row justify-start items-center" target="_blank">View Resume</a>
+        {/*<a href="/Yevhen_Kaznovskyi_CV2023.pdf" className="w-full h-[40px] text-left flex flex-row justify-start items-center" target="_blank">View Resume</a>*/}
       </div>
     </div>
   </div>
