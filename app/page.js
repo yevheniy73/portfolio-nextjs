8ac81522de1b7bd5f2ff22ec2caf67615f910f7a
yevheniy73 <71726440+yevheniy73@users.noreply.github.com>
2023-05-24 00:06:18 -0600

    home responsive finished

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 import { Hero } from '../sections';
 
 const Page = () => (
-  <div className='w-full h-auto flex items-center my-755:px-[125px] my-500:px-[50px] px-[25px]'>
+  <div className='w-full h-auto flex items-center my-755:px-[125px] my-500:px-[50px] p-[25px]'>
     <Hero />
   </div>
 );
--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -23,35 +23,39 @@ const Hero = () => {
   const opacities = [60, 80, 100, 80, 60];
 
   return (
-    <section id="hero" className="w-full flex justify-between items-center gap-8 flex-row">
-      <div className="z-10">
-        <div className="css-blurry-gradient-blue"></div>
-        <div className="css-blurry-gradient-green"></div>
-        <div className="head relative z-[11]">
-          <span className="text-white text-[18px]">Hi, I am</span>
+    <section id="hero" className="w-full h-full flex justify-between items-center gap-8 flex-row">
+      <div className="css-blurry-gradient-blue"></div>
+      <div className="css-blurry-gradient-green"></div>
 
-          <h2 className="text-white my-500:text-[58px] text-[44px] my-md:max-w-full my-500:max-w-[260px] max-w-[200px] my-md:h-auto h-[175px]">
-            <Typewriter
-              options={{
-                strings: ['Yevhen Kaznovskyi'],
-                autoStart: true,
-                loop: true,
-                delay: 120,
-                pauseFor: 1800,
-              }}
-            />
-          </h2>
+      <div className="head relative z-[11] ">
+        <span className="text-white text-[18px]">Hi, I am</span>
 
-          <h2 className="text-secondary-purple sm:text-[32px] text-[22px]">&gt; Software Developer</h2>
+        <h2 className="text-white my-500:text-[58px] text-[44px] my-md:max-w-full my-500:max-w-[260px] max-w-[200px] my-md:h-auto my-500:h-[175px] h-[130px]">
+          <Typewriter
+            options={{
+              strings: ['Yevhen Kaznovskyi'],
+              autoStart: true,
+              loop: true,
+              delay: 120,
+              pauseFor: 1800,
+            }}
+          />
+        </h2>
 
-          <p className="mt-[80px]">// view my resume</p>
-          <span className="text-white inline-flex whitespace-pre"><p className="text-secondary-purple">const </p><p className="text-secondary-green">resume </p>=
-            <a href="/Yevhen_Kaznovskyi_CV.pdf" target="_blank" className="sm:block hidden my-resume text-accent-peach">" https://yevhen.vercel.app/resume"</a>
-            <a href="/Yevhen_Kaznovskyi_CV.pdf" target="_blank" class="sm:hidden block my-resume text-accent-peach">" /resume"</a>
+        <h2 className="my-755:text-secondary-purple text-accent-green sm:text-[32px] text-[22px]">&gt; Software Developer</h2>
+        
+        <div className="block my-500:relative absolute my-500:bottom-0 bottom-[60px] my-500:mt-[80px] mt-0">
+          <p>// view my resume</p>
+          <span className="text-white inline-flex whitespace-pre"><p className="text-secondary-purple">const </p><p className="text-accent-green">resume </p><p>= </p>
+            <a href="/Yevhen_Kaznovskyi_CV.pdf" target="_blank" className="my-resume text-accent-peach">
+              <p className="sm:block hidden">"https://yevhen.vercel.app/resume"</p>
+              <p className="sm:hidden block">"/resume"</p>
+            </a>
           </span>
+          </div>
 
-        </div>
       </div>
+
       <div className="max-h-[180px] z-10 my-1320:block hidden relative top-0">
         <div className="embla">
         <div className="embla__viewport" ref={emblaRef}>
